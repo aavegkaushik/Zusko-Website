7e6ed5d76c5b7c99b94c07d828c03c3d606d0935
AAVEGKAUSHIK <aavegkaushik@gmail.com>
2026-05-27 21:33:25 +0530

    Add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,14 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { CartProvider } from './context/CartContext.jsx'
-
+import { Analytics } from "@vercel/analytics/react";
 createRoot(document.getElementById('root')).render(
     <AuthProvider>
     <CartProvider>
       <BrowserRouter>
         <StrictMode>
           <App />
+          <Analytics />
         </StrictMode>
       </BrowserRouter>
     </CartProvider>
